fba7f9ce81f31838b766127da7f57f642115600f
jokerwrld999 <ifalaleev49@gmail.com>
2023-06-15 17:40:04 +0300

    [ci/cd] Build

--- run/start.sh
+++ run/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 pipeline=$(pwd)
-echo $(ls $pipeline/build/libs/)
-#java -jar -Dserver.port=8090 -Dspring.profiles.active=mongodb $pipeline/build/libs/spring-music-1.0.jar & echo $! > ./pid1.file &
-#java -jar -Dserver.port=8091 -Dspring.profiles.active=mongodb $pipeline/build/libs/spring-music-1.0.jar & echo $! > ./pid2.file &
+java -jar -Dserver.port=8090 -Dspring.profiles.active=mongodb $pipeline/build/libs/spring-music-1.0.jar & echo $! > ./pid1.file &
+java -jar -Dserver.port=8091 -Dspring.profiles.active=mongodb $pipeline/build/libs/spring-music-1.0.jar & echo $! > ./pid2.file &
