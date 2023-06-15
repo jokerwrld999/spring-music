ee2bb9816223aebc1b11a3d5078a3492d24dc7cc
jokerwrld999 <ifalaleev49@gmail.com>
2023-06-15 17:29:40 +0300

    [ci/cd] Build

--- run/start.sh
+++ run/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+pwd
 java -jar -Dserver.port=8090 -Dspring.profiles.active=mongodb ./build/libs/spring-music-1.0.jar & echo $! > ./pid1.file &
 java -jar -Dserver.port=8091 -Dspring.profiles.active=mongodb ./build/libs/spring-music-1.0.jar & echo $! > ./pid2.file &
