a41b73db97b3a83cd5f46d9ca377fef7821e642e
jokerwrld999 <ifalaleev49@gmail.com>
2023-06-16 11:54:35 +0300

    [ci/cd] App service fix

--- custom-configs/spring-music-service/start.sh
+++ custom-configs/spring-music-service/start.sh
@@ -2,5 +2,5 @@
 
 WORKING_DIR=$(echo $HOME/spring-music-app/)
 
-/usr/bin/java -jar -Dserver.port=8090 -Dspring.profiles.active=mongodb $WORKING_DIR/spring-music-1.0.jar & echo $! > $WORKING_DIR/pid1.file & > /dev/null 2>&1
+java -jar -Dserver.port=8090 -Dspring.profiles.active=mongodb $WORKING_DIR/spring-music-1.0.jar & echo $! > $WORKING_DIR/spring-music-service/pid1.file & > /dev/null 2>&1
 #java -jar -Dserver.port=8091 -Dspring.profiles.active=mongodb $artifact & echo $! > ./pid2.file &
--- custom-configs/spring-music-service/stop.sh
+++ custom-configs/spring-music-service/stop.sh
@@ -2,5 +2,5 @@
 
 WORKING_DIR=$(echo $HOME/spring-music-app/)
 
-kill $(cat $WORKING_DIR/pid1.file)
+kill $(cat $WORKING_DIR/spring-music/spring-music-service/pid1.file)
 #kill $(cat ./pid2.file)
