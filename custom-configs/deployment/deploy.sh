45bb3a1bdcd0cf289a62b52bdc06d4040a97e984
jokerwrld999 <ifalaleev49@gmail.com>
2023-06-16 12:31:27 +0300

    [ci/cd] App service fix

--- custom-configs/deployment/deploy.sh
+++ custom-configs/deployment/deploy.sh
@@ -6,12 +6,10 @@ APP_DIR=$HOME/spring-music-app
 echo "  ----- setup base directory -----  "
 mkdir $APP_DIR
 yes | cp -rf $ARTIFACT $APP_DIR/
-yes | cp -rf $SRC/custom-configs/spring-music-service $APP_DIR/
 sudo chmod 500 $APP_DIR/spring-music-1.0.jar
 
 echo "  ----- copy unit file for application -----  "
 sudo mv -f $SRC/custom-configs/spring-music-service/spring-music.service /etc/systemd/system/spring-music.service
 
 echo "  ----- start the application -----  "
-sudo systemctl start spring-music
-sudo systemctl enable spring-music
+sudo systemctl enable --now spring-music
